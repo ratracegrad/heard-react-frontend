cfc95bdd1ddb06e34234a4e616533e873003e4f2
Jennifer Bland <ratracegrad@gmail.com>
2023-12-28 14:37:51 -0500

    Show transactions and use typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { useState, useEffect } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   const [transactions, setTransactions] = useState([]);
+
+  interface Transaction {
+    id: number;
+    title: string;
+    description: string;
+    amount: number;
+    fromAccount: string;
+    toAccount: string;
+    transactionDate: string;
+  }
 
   useEffect(() => {
     fetch('https://octopus-app-ft9df.ondigitalocean.app/transactions')
@@ -20,26 +27,83 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <h2 className="mx-auto mt-10 text-center text-3xl text-gray-900 font-bold">
+        Transactions List
+      </h2>
+    <div className="flex justify-end">
+      <button className="mr-2 rounded bg-gray-300 px-4 py-2 text-black font-bold hover:bg-gray-500">
+        + Add
+      </button>
+    </div>
+    <div>
+      <table className="mx-auto mt-10 border-collapse table-auto border border-gray-400">
+        <thead>
+          <tr>
+            <th className="border border-gray-400 px-4 py-2">
+              ID
+            </th>
+            <th className="border border-gray-400 px-4 py-2">
+              Title
+            </th>
+            <th className="border border-gray-400 px-4 py-2">
+              Description
+            </th>
+            <th className="border border-gray-400 px-4 py-2">
+              Amount
+            </th>
+            <th className="border border-gray-400 px-4 py-2">
+              From Account
+            </th>
+            <th className="border border-gray-400 px-4 py-2">
+              To Account
+            </th>
+            <th className="border border-gray-400 px-4 py-2">
+              Transaction Date
+            </th>
+            <th className="border border-gray-400 px-4 py-2">
+              Actions
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {transactions.map((transaction: Transaction) => (
+            <tr key={transaction.id}>
+              <td className="border border-gray-400 px-4 py-2">
+                {transaction.id}
+              </td>
+              <td className="border border-gray-400 px-4 py-2">
+                {transaction.title}
+              </td>
+              <td className="border border-gray-400 px-4 py-2">
+                {transaction.description}
+              </td>
+              <td className="border border-gray-400 px-4 py-2">
+                {transaction.amount}
+              </td>
+              <td className="border border-gray-400 px-4 py-2">
+                {transaction.fromAccount}
+              </td>
+              <td className="border border-gray-400 px-4 py-2">
+                {transaction.toAccount}
+              </td>
+              <td className="border border-gray-400 px-4 py-2">
+                {transaction.transactionDate}
+              </td>
+              <td className="border border-gray-400 px-4 py-2">
+                <button className="mr-2 rounded bg-blue-500 px-4 py-2 text-white font-bold hover:bg-blue-700">
+                  Edit
+                </button>
+                <button className="rounded bg-red-500 px-4 py-2 text-white font-bold hover:bg-red-700">
+                  Remove
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  </div>
     </>
   )
 }
